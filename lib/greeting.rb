fb7e626aa1f672a246b454b59b0cc2796fd85e9a
Kristyn Little <kristynlittle@frontier.com>
2020-12-20 22:35:50 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,3 +1,6 @@
 def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
+  puts "Please enter your name sot that we can greet you more personally:"
+  name = gets.strip
+  greeting(name)
   
